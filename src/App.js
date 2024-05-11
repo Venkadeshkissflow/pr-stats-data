97a12f85f8b46807c146f582b79fd97cd72e9943
Venkadesh S <venkadesh@kissflow.com>
2024-05-11 15:14:32 +0530

    change26

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          kissflow change25 <code>src/App.js</code> demo
+          kissflow change26 <code>src/App.js</code> demo
         </p>
         <a
           className="App-link"
